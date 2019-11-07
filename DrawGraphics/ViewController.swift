c9b054b44c2e9bf42439b5fdded89cf3d80f817f
juanagustin0504 <juanagustin@naver.com>
2019-11-07 16:13:25 +0900

    add : draw arc

--- DrawGraphics/ViewController.swift
+++ DrawGraphics/ViewController.swift
@@ -81,6 +81,25 @@ class ViewController: UIViewController {
     }
     
     @IBAction func btnDrawArc(_ sender: UIButton) {
+        UIGraphicsBeginImageContext(imgView.frame.size)
+        let context = UIGraphicsGetCurrentContext()!
+        
+        // Draw Arc
+        context.setLineWidth(5.0)
+        context.setStrokeColor(UIColor.red.cgColor)
+        
+        context.move(to: CGPoint(x: 50, y: 50))
+        context.addArc(tangent1End: CGPoint(x: 200, y: 50), tangent2End: CGPoint(x: 200, y: 200), radius: CGFloat(50))
+        context.addLine(to: CGPoint(x: 200, y: 200))
+        
+        context.move(to: CGPoint(x: 100, y: 250))
+        context.addArc(tangent1End: CGPoint(x: 250, y: 250), tangent2End: CGPoint(x: 100, y: 400), radius: CGFloat(20))
+        context.addLine(to: CGPoint(x: 100, y: 400))
+        
+        context.strokePath()
+        
+        imgView.image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
     }
     
     @IBAction func btnDrawFill(_ sender: UIButton) {
